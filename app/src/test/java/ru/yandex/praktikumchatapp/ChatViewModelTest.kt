7f0deabc8ef4d3b6c87c3313cb23f0d8d3577e9e
lbonifatsiy <lev.bonifatsiy@yandex.ru>
2025-08-03 22:21:15 +0300

    test single message sending

--- app/src/test/java/ru/yandex/praktikumchatapp/ChatViewModelTest.kt
+++ app/src/test/java/ru/yandex/praktikumchatapp/ChatViewModelTest.kt
@@ -2,9 +2,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
+import org.hamcrest.CoreMatchers.hasItem
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -32,7 +35,9 @@ class ChatViewModelTest {
     @Test
     fun `send message should update messages with MyMessage`() = runTest {
         val message = Message.MyMessage("TestMessage")
-
+        viewModel.sendMyMessage(message.text)
+        advanceUntilIdle()
+        assertThat(viewModel.messages.value, hasItem(message))
     }
 
     @Test
