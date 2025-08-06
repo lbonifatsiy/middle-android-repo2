9bb7a6a71965ff8a3aa2441292f6cf8db0bd5535
lbonifatsiy <lev.bonifatsiy@yandex.ru>
2025-08-06 21:06:03 +0300

    refactor chat api

--- app/src/main/java/ru/yandex/praktikumchatapp/data/ChatApi.kt
+++ app/src/main/java/ru/yandex/praktikumchatapp/data/ChatApi.kt
@@ -9,10 +9,6 @@ import kotlin.random.Random
 
 class ChatApi {
 
-    companion object {
-        private const val MAXIMUM_RESPONSE_DELAY = 5000L
-    }
-
     private val responses = listOf(
         "Отлично!",
         "Понял.",
@@ -89,10 +85,14 @@ class ChatApi {
     fun getReply(): Flow<String> = flow {
         while (currentCoroutineContext().isActive) {
             delay(Random.nextLong(MAXIMUM_RESPONSE_DELAY))
-//        if (Random.nextBoolean()) {
-//            throw Exception("Ошибка запроса", Throwable("Something went wrong"))
-//        }
+            if (Random.nextBoolean()) {
+                throw Exception("Ошибка запроса", Throwable("Something went wrong"))
+            }
             emit(responses.random())
         }
+    }
+
+    private companion object {
+        const val MAXIMUM_RESPONSE_DELAY = 5000L
     }
 }
