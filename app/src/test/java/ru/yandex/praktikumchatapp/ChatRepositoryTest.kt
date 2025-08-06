3dcfaf08763f4696c4f4342be31625cc08a909b7
lbonifatsiy <lev.bonifatsiy@yandex.ru>
2025-08-06 21:07:25 +0300

    add chat repository test

--- app/src/test/java/ru/yandex/praktikumchatapp/ChatRepositoryTest.kt
+++ app/src/test/java/ru/yandex/praktikumchatapp/ChatRepositoryTest.kt
@@ -4,6 +4,7 @@ import app.cash.turbine.test
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.resetMain
@@ -13,14 +14,13 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito.mock
-import org.mockito.Mockito.times
 import org.mockito.Mockito.`when`
-import org.mockito.kotlin.verify
 import ru.yandex.praktikumchatapp.data.ChatApi
 import ru.yandex.praktikumchatapp.data.ChatRepository
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class ChatRepositoryTest {
+
     private var testDispatcher: TestDispatcher = StandardTestDispatcher()
 
     private lateinit var chatRepository: ChatRepository
@@ -39,7 +39,15 @@ class ChatRepositoryTest {
 
     @Test
     fun `getReplyMessage should return a non-empty string`() = runTest {
+        val replyText = "Hello"
 
+        `when`(chatApi.getReply())
+            .thenReturn(flowOf("Hello"))
+
+        chatRepository.getReplyMessage().test {
+            assert(awaitItem() == replyText)
+            cancelAndIgnoreRemainingEvents()
+        }
     }
 
     @Test
